9347f35db4afbf7ae92f288cbf419e558b2097ff
bene-god <egbadonjeffrey@gmail.com>
2022-07-17 12:36:16 +0100

    Completed Feedback app

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <div>
+      <h1>Post</h1>
+      <button onClick={onClick}>Click</button>
+    </div>
+  );
+};
+
+export default Post;
