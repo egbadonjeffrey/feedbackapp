38d30789b484ab271b4c7bd51c67569127cde0f0
bene-god <egbadonjeffrey@gmail.com>
2022-07-17 12:38:18 +0100

    Completed Feedback App

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FaTimes, FaEdit } from "react-icons/fa";
+import Card from "./shared/Card";
+import { useContext } from "react";
+import { FeedbackContext } from "../context/FeedbackContext";
+
+const FeedbackItem = ({ item }) => {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
+  return (
+    <Card reverse={false}>
+      <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={() => deleteFeedback(item.id)}>
+        <FaTimes color="purple" />
+      </button>
+      <button className="edit" onClick={() => editFeedback(item)}>
+        <FaEdit color="purple" />
+      </button>
+      <div className="text-display"> {item.text} </div>
+    </Card>
+  );
+};
+
+export default FeedbackItem;
